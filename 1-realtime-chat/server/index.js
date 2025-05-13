e99a999d553e05ab5d1479a3c0aaeee711ef78c2
Jordan Herrera <jordan.herreravera@gmail.com>
2025-05-12 18:14:40 -0600

    feat: implement username prompt and message storage in database

--- 1-realtime-chat/server/index.js
+++ 1-realtime-chat/server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
-
+import dotenv from 'dotenv';
+import {createClient} from '@libsql/client';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
-
+dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
@@ -13,12 +14,47 @@ const io = new Server(server, {
 connectionStateRecovery: {},
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+const db = createClient({
+    url: process.env.DB_URL,
+    authToken: process.env.DB_TOKEN,
+});
 
-    socket.on('chat message', (msg) => {
-        console.log('Message: ' + msg);
-        io.emit('chat message', msg);
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        message TEXT NOT NULL,
+        user TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `)
+
+io.on('connection', async (socket) => {
+    console.log('A user connected');
+   
+
+
+    socket.on('chat message', async (msg) => {
+        let result;
+        
+        
+        try{
+            const username = socket.handshake.auth.username ?? 'Anonymous';
+            if (!msg) {
+                console.error('Message is empty');
+                return;
+            }
+            result = await db.execute(
+                {
+                    sql: 'INSERT INTO messages (message, user) VALUES (:msg, :username)',
+                    args: { msg, username },
+                }
+            );
+        }catch (error) {
+            console.error('Error inserting message:', error);
+            return;
+        }
+;
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), socket.handshake.auth.username ?? 'Anonymous');
     });
 
 
@@ -26,6 +62,22 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
+
+    if(!socket.recovered){
+      
+        try {
+            const results = await db.execute(
+                {
+                    sql: 'SELECT * FROM messages ORDER BY created_at DESC LIMIT 10',
+                }
+            );
+        socket.emit("chat history", results);
+        }catch (error) {
+            console.error('Error fetching messages:', error);
+            return;
+        }
+       
+    }
 
 });
 
