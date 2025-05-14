90ecfce2b977f045ee2daae1d10cdeb6b7df96ab
Jordan Herrera <jordan.herreravera@gmail.com>
2025-05-13 19:03:59 -0600

    feat: enhance authentication flow with JWT, add user validation, and implement login/register views

--- 2-auth-cookies-jwt/index.js
+++ 2-auth-cookies-jwt/index.js
@@ -1,25 +1,68 @@
 import express from 'express'
-import { PORT } from './config.js'
+import cookieParser from 'cookie-parser'
+import { PORT, JWT_SECRET } from './config.js'
 import { UserRepository } from './user-repository.js'
 import { ErrorValidation } from './utils/errorValidation.js'
+import jwt from 'jsonwebtoken'
 const app = express()
+app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  let data = null
+  req.session = { user: null }
+  try {
+    data = jwt.verify(token, JWT_SECRET)
+    req.session.user = data
+  } catch (e) {
+    req.session.user = null
+  }
+
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  try {
+    const { user } = req.session
+    res.render('index', user)
+  } catch (e) {
+    console.log(e)
+    return res.clearCookie('access_token').render('index')
+  }
 })
 
-app.get('/login', (req, res) => {
-  res.json({
-    message: 'Login Page',
-    user: 'Anonymous'
-  })
-})
-
-app.get('/register', (req, res) => {
+app.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body
-    const id = UserRepository.create({ username, password })
+    const user = await UserRepository.login({ username, password })
+    const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '1h' })
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict',
+      maxAge: 3600000 // 1 hour
+    }
+    ).send({ user, token })
+  } catch (e) {
+    console.log(e)
+    if (e instanceof ErrorValidation) {
+      res.status(e.statusCode).json({
+        message: e.message
+      })
+    } else {
+      res.status(500).json({
+        message: 'Internal Server Error'
+      })
+    }
+  }
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const id = await UserRepository.create({ username, password })
     res.json({
       message: 'User created successfully',
       user: { id, username }
@@ -37,12 +80,21 @@ app.get('/register', (req, res) => {
   }
 })
 
-app.get('/logout', (req, res) => {
-  res.send('Logout Page')
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token')
+  res.redirect('/')
 })
 
 app.get('/protected', (req, res) => {
-  // protected route
+  const { user } = req.session
+  if (!user) {
+    return res.status(401).send('Unauthorized')
+  }
+  try {
+    res.render('protected', user)
+  } catch (e) {
+    return res.status(401).send('Unauthorized')
+  }
 })
 
 app.listen(PORT, () => {
--- 2-auth-cookies-jwt/user-repository.js
+++ 2-auth-cookies-jwt/user-repository.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt'
 import crypto from 'node:crypto'
 import DBLocal from 'db-local'
 import { ErrorValidation } from './utils/errorValidation.js'
+import { SALT_ROUNDS } from './config.js'
 const { Schema } = new DBLocal({ path: './db' })
 
 const User = Schema('User', {
@@ -11,22 +12,41 @@ const User = Schema('User', {
 })
 
 export class UserRepository {
-  static create ({ username, password }) {
-    if (typeof username !== 'string') throw new ErrorValidation('username must be a string', 400)
-    if (typeof password !== 'string') throw new ErrorValidation('password must be a string', 400)
-    if (username.length < 3) throw new ErrorValidation('username must be at least 3 characters long', 400)
-    if (password.length < 6) throw new ErrorValidation('password must be at least 6 characters long', 400)
-
+  static async create ({ username, password }) {
+    // validations
+    Validation.username(username)
+    Validation.password(password)
     const user = User.findOne({ username })
     if (user) throw new ErrorValidation('username already exists', 400)
 
     const id = crypto.randomUUID()
-    const hashedPassword = bcrypt.hashSync(password, 10)
-    User.create({ _id: id, username, hashedPassword }).save()
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+    User.create({ _id: id, username, password: hashedPassword }).save()
     return id
   }
 
-  static login ({ username, password }) {
-    // validations
+  static async login ({ username, password }) {
+    Validation.username(username)
+    Validation.password(password)
+    const user = User.findOne({ username })
+    if (!user) throw new ErrorValidation('username does not exists', 404)
+
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) throw new ErrorValidation('password is incorrect', 401)
+
+    const { password: _, ...userWithoutPassword } = user
+
+    return userWithoutPassword
+  }
+}
+class Validation {
+  static username (username) {
+    if (typeof username !== 'string') throw new ErrorValidation('username must be a string', 400)
+    if (username.length < 3) throw new ErrorValidation('username must be at least 3 characters long', 400)
+  }
+
+  static password (password) {
+    if (typeof password !== 'string') throw new ErrorValidation('password must be a string', 400)
+    if (password.length < 6) throw new ErrorValidation('password must be at least 6 characters long', 400)
   }
 }
